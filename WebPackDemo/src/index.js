0cda42a8fc8cf7a0ae4c26ff5098cf4ad0c59ac2
bryanlihp <bryanlihp@hotmail.com>
2018-06-22 11:47:29 -0700

    Add project files.

---
+++ WebPackDemo/src/index.js
@@ -0,0 +1,12 @@
+﻿import _ from 'lodash';
+
+function component() {
+    var element = document.createElement('div');
+
+    // Lodash, now implemented by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
